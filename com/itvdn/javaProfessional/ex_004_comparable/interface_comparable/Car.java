8663f935cd0d7a5c2dec872941f3c4cd5cdd4d0a
Sergey Khaykov <ssserg3000@gmail.com>
2023-01-15 17:04:59 +0200

    Initial commit

---
+++ com/itvdn/javaProfessional/ex_004_comparable/interface_comparable/Car.java
@@ -0,0 +1,47 @@
+package com.itvdn.javaProfessional.ex_004_comparable.interface_comparable;
+
+public class Car implements Comparable { //<Car>{
+    int speed;
+    int price;
+    String model;
+    String color;
+
+    Car(int speed, int price, String model, String color) {
+        this.speed = speed;
+        this.price = price;
+        this.model = model;
+        this.color = color;
+    }
+
+    public String toString() {
+        return this.model + " " + this.color + " " + this.speed + " " + this.price;
+    }
+
+    // Сортируем по скорости/цене
+    @Override
+    public int compareTo(Object o) {    // public int compareTo(Car o) {
+        int temp = this.speed - ((Car)o).speed;   // int temp = this.speed - o.speed;
+        if (temp == 0) {
+            temp = this.price - ((Car)o).price;
+            if (temp == 0){
+                temp = this.model.compareTo(((Car)o).model);
+                if (temp == 0){
+                    temp = this.color.compareTo(((Car)o).color);
+                    if (temp == 0){
+                        return 0;
+                    }
+                    return temp;
+
+                }
+                return temp;
+
+            }
+            return temp;
+        } else {
+            return temp;
+        }
+    }
+
+    // Сравнение 2-х строковых значений
+    // return this.model.compareTo((Car)o).model);
+}
